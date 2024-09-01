f1da8c39a00a811450a22882ed8ff3730e38c1d3
CinnabonK <138283495+CinnabonK@users.noreply.github.com>
2024-09-01 22:23:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -49,28 +49,23 @@ function moveTiles(direction) {
     let moved = false;
     let merged = Array(gridSize).fill(null).map(() => Array(gridSize).fill(false));
 
-    function move(x, y, newX, newY) {
-        if (grid[newX][newY] === grid[x][y] && !merged[newX][newY] && !merged[x][y]) {
-            grid[newX][newY] *= 2;
-            grid[x][y] = 0;
-            merged[newX][newY] = true;
-            moved = true;
-        } else if (grid[newX][newY] === 0) {
-            grid[newX][newY] = grid[x][y];
-            grid[x][y] = 0;
-            moved = true;
-        }
-    }
-
     switch (direction) {
         case 'up':
             for (let y = 0; y < gridSize; y++) {
                 for (let x = 1; x < gridSize; x++) {
                     if (grid[x][y] !== 0) {
                         let newX = x;
-                        while (newX > 0) {
-                            move(newX, y, newX - 1, y);
+                        while (newX > 0 && grid[newX - 1][y] === 0) {
+                            grid[newX - 1][y] = grid[newX][y];
+                            grid[newX][y] = 0;
                             newX--;
+                            moved = true;
+                        }
+                        if (newX > 0 && grid[newX - 1][y] === grid[newX][y] && !merged[newX - 1][y]) {
+                            grid[newX - 1][y] *= 2;
+                            grid[newX][y] = 0;
+                            merged[newX - 1][y] = true;
+                            moved = true;
                         }
                     }
                 }
@@ -81,9 +76,17 @@ function moveTiles(direction) {
                 for (let x = gridSize - 2; x >= 0; x--) {
                     if (grid[x][y] !== 0) {
                         let newX = x;
-                        while (newX < gridSize - 1) {
-                            move(newX, y, newX + 1, y);
+                        while (newX < gridSize - 1 && grid[newX + 1][y] === 0) {
+                            grid[newX + 1][y] = grid[newX][y];
+                            grid[newX][y] = 0;
                             newX++;
+                            moved = true;
+                        }
+                        if (newX < gridSize - 1 && grid[newX + 1][y] === grid[newX][y] && !merged[newX + 1][y]) {
+                            grid[newX + 1][y] *= 2;
+                            grid[newX][y] = 0;
+                            merged[newX + 1][y] = true;
+                            moved = true;
                         }
                     }
                 }
@@ -94,9 +97,17 @@ function moveTiles(direction) {
                 for (let y = 1; y < gridSize; y++) {
                     if (grid[x][y] !== 0) {
                         let newY = y;
-                        while (newY > 0) {
-                            move(x, newY, x, newY - 1);
+                        while (newY > 0 && grid[x][newY - 1] === 0) {
+                            grid[x][newY - 1] = grid[x][newY];
+                            grid[x][newY] = 0;
                             newY--;
+                            moved = true;
+                        }
+                        if (newY > 0 && grid[x][newY - 1] === grid[x][newY] && !merged[x][newY - 1]) {
+                            grid[x][newY - 1] *= 2;
+                            grid[x][newY] = 0;
+                            merged[x][newY - 1] = true;
+                            moved = true;
                         }
                     }
                 }
@@ -107,9 +118,17 @@ function moveTiles(direction) {
                 for (let y = gridSize - 2; y >= 0; y--) {
                     if (grid[x][y] !== 0) {
                         let newY = y;
-                        while (newY < gridSize - 1) {
-                            move(x, newY, x, newY + 1);
+                        while (newY < gridSize - 1 && grid[x][newY + 1] === 0) {
+                            grid[x][newY + 1] = grid[x][newY];
+                            grid[x][newY] = 0;
                             newY++;
+                            moved = true;
+                        }
+                        if (newY < gridSize - 1 && grid[x][newY + 1] === grid[x][newY] && !merged[x][newY + 1]) {
+                            grid[x][newY + 1] *= 2;
+                            grid[x][newY] = 0;
+                            merged[x][newY + 1] = true;
+                            moved = true;
                         }
                     }
                 }
